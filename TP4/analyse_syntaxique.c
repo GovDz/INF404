7633e612881fd97a58742ab53c9f40c92dbba513
GovDz <f_boudellal@estin.dz>
2024-02-18 19:08:47 +0100

    main

--- TP4/analyse_syntaxique.c
+++ TP4/analyse_syntaxique.c
@@ -8,115 +8,95 @@
 #include "ast_parcours.h"
 #include "analyse_lexicale.h"
 
-TypeOperateur Operateur(Nature_Lexeme nature); // si on met cette fonction dans analyse_lexicale.h, on a une erreur de compilation
-Ast rec_eag() { // d'apres le cours
-    Ast A1;
-    seq_terme(&A1);
-    return A1;
+TypeOperateur Operateur(Nature_Lexeme nature);
+
+void rec_eag(Ast *A1) {
+    seq_terme(A1);
 }
 
-Ast seq_terme() { // d'apres le cours
-    Ast A2;
+void seq_terme(Ast *A2) {
     Ast A1;
     terme(&A1);
-    return suite_seq_terme(A1, &A2);
+    suite_seq_terme(A1, A2);
 }
 
-Ast suite_seq_terme(Ast A1, Ast *A2) { // d'apres le cours
+void suite_seq_terme(Ast A1, Ast *A2) {
     Ast A3, A4;
     TypeOperateur op;
 
     if (op1(&op)) {
         terme(&A3);
         A4 = creer_operation(op, A1, A3);
-        return suite_seq_terme(A4, A2);
+        suite_seq_terme(A4, A2);
     } else {
         *A2 = A1;
-        return *A2;
     }
 }
 
-Ast terme() { // d'apres le cours
-    Ast A1;
-    seq_facteur(&A1);
-    return A1;
+void terme(Ast *A1) {
+    seq_facteur(A1);
 }
 
-Ast seq_facteur() { // d'apres le cours
-    Ast A2;
+void seq_facteur(Ast *A2) {
     Ast A1;
     facteur(&A1);
-    return suite_seq_facteur(A1, &A2);
+    suite_seq_facteur(A1, A2);
 }
 
-Ast suite_seq_facteur(Ast A1, Ast *A2) { // d'apres le cours
+void suite_seq_facteur(Ast A1, Ast *A2) {
     Ast A3, A4;
     TypeOperateur op;
-    int r = op2(&op);
 
-    if (r != 0) {
+    if (op2(&op)) {
         facteur(&A3);
         A4 = creer_operation(op, A1, A3);
-        return suite_seq_facteur(A4, A2);
+        suite_seq_facteur(A4, A2);
     } else {
         *A2 = A1;
-        return *A2;
     }
 }
 
-Ast facteur() {
-    Ast A1;
-
+void facteur(Ast *A1) {
     switch (lexeme_courant().nature) {
         case ENTIER:
-            A1 = creer_valeur(lexeme_courant().valeur);
+            *A1 = creer_valeur(lexeme_courant().valeur);
             avancer();
             break;
         case PARO:
             avancer();
-            rec_eag(&A1);
+            rec_eag(A1);
             if (lexeme_courant().nature == PARF) {
                 avancer();
             } else {
-                printf("ERREUR : parenthese fermante attendue (ligne %u, colonne %u)\n",
-                       lexeme_courant().ligne, lexeme_courant().colonne);
-                exit(1);
+                handle_error("parenthese fermante attendue");
             }
             break;
         default:
-            printf("ERREUR : entier ou parenthese ouvrante attendu (ligne %u, colonne %u)\n",
-                   lexeme_courant().ligne, lexeme_courant().colonne);
-            exit(0);
+            handle_error("entier ou parenthese ouvrante attendu");
     }
-
-    return A1;
 }
 
-TypeOperateur op1() {
-    TypeOperateur Op;
-
+int op1(TypeOperateur *Op) {
     switch (lexeme_courant().nature) {
         case PLUS:
         case MOINS:
-            Op = Operateur(lexeme_courant().nature);
+            *Op = Operateur(lexeme_courant().nature);
             avancer();
-            return Op;
+            return 1;
         default:
-            return -1;  // Or any value indicating no operator found
+            return 0;
     }
 }
 
-TypeOperateur op2() {
-    TypeOperateur Op;
-
+int op2(TypeOperateur *Op) {
     switch (lexeme_courant().nature) {
         case MUL:
         case DIV:
-            Op = Operateur(lexeme_courant().nature);
+            *Op = Operateur(lexeme_courant().nature);
             avancer();
-            return Op;
+            return 1;
         default:
-            return -1;  // Or any value indicating no operator found
+            return 0;
     }
 }
 
@@ -131,25 +111,25 @@ TypeOperateur Operateur(Nature_Lexeme nature) {
         case DIV:
             return N_DIV;
         default:
-            printf("Erreur operateur");
-            exit(1);
+            handle_error("Erreur operateur");
     }
 }
-
-/* ----------------------------------------------------------------------- */
-
 int analyser(char *nomFichier, Ast *A) {
     demarrer(nomFichier);
-    *A = rec_eag();
+    rec_eag(A);
 
-    if ((lexeme_courant().nature == FIN_SEQUENCE)) {
-        /* syntaxe correcte */
+    if (lexeme_courant().nature == FIN_SEQUENCE) {
         printf("\nSyntaxe correcte\n");
-        afficherA(*A); // on affiche l'AST
+        afficherA(*A);
         printf("\n");
         return 1;
     } else {
-        printf("Erreur lors de l'analyse syntaxique ...\n");
+        handle_error("Erreur lors de l'analyse syntaxique");
         return 0;
     }
+}
+
+void handle_error(const char *message) {
+    printf("Erreur : %s\n", message);
+    exit(1);
 }
--- TP4/analyse_syntaxique.h
+++ TP4/analyse_syntaxique.h
@@ -6,15 +6,15 @@
 #include "ast_construction.h"
 #include "ast_parcours.h"
 
-Ast rec_eag();
-Ast seq_terme();
-Ast suite_seq_terme(Ast A1, Ast *A2);
-Ast terme();
-Ast seq_facteur();
-Ast suite_seq_facteur(Ast A1, Ast *A2);
-Ast facteur();
-TypeOperateur op1();
-TypeOperateur op2();
+void rec_eag(Ast *A1);
+void seq_terme(Ast *A2);
+void suite_seq_terme(Ast A1, Ast *A2);
+void terme(Ast *A1);
+void seq_facteur(Ast *A2);
+void suite_seq_facteur(Ast A1, Ast *A2);
+void facteur(Ast *A1);
+int op1(TypeOperateur *Op);
+int op2(TypeOperateur *Op);
 
 #endif  // ANALYSE_SYNTAXIQUE_H
 
