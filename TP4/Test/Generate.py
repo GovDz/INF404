a0d3f8a0af5934e7647dc40b06b27ed47b85504f
GovDz <f_boudellal@estin.dz>
2024-02-24 23:13:25 +0100

    main

--- TP4/Test/Generate.py
+++ TP4/Test/Generate.py
@@ -1,23 +1,30 @@
 import random
 import os
 
-def generate_arithmetic_expression(valid=True):
+def generate_arithmetic_expression():
     operators = ['+', '-', '*', '/']
     
-    if not valid:
-        return "This is an invalid expression"
-    
-    expression = ''
-    for _ in range(random.randint(1, 10)):
+    if random.random() < 0.5:
+        expression = ''
+        for _ in range(random.randint(1, 10)):
+            expression += str(random.randint(1, 100))
+            expression += random.choice(operators)
         expression += str(random.randint(1, 100))
-        expression += random.choice(operators)
-    expression += str(random.randint(1, 100))
-    return expression
+        return expression
+    else:
+        return generate_random_invalid_expression()
+
+def generate_random_invalid_expression():
+    operators = ['+', '-', '*', '/']
+    invalid_characters = ['(', ')', '*']
+    invalid_expression = ''.join(str(random.choice(invalid_characters)) for _ in range(random.randint(1, 10)))
+    invalid_expression += random.choice(operators)
+    
+    return invalid_expression
 
 def generate_test_file(num_tests):
     for i in range(1, num_tests + 1):
-        is_valid = random.random() < 0.5
-        expression = generate_arithmetic_expression(is_valid)
+        expression = generate_arithmetic_expression()
         file_name = f'Test_{i}.test'
         with open(file_name, 'w') as file:
             file.write(expression)
